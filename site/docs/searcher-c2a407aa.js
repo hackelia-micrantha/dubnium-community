095ce31c903a36c9c6ed1773a99aff1dc81ac3eb
dubnium-book-publisher <actions@users.noreply.github.com>
2026-08-03 15:32:14 -0700

    Publish Dubnium book from 0082f1471615

--- site/docs/searcher-c2a407aa.js
+++ site/docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d035b8e1.js',
+                path_to_root + 'searchindex-196f02b5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
